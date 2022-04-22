3923e5fed858f120a4000b59ae7ad8ca2dbd38d6
Rol John Torralba <roljohntorralba@gmail.com>
2022-04-22 12:22:20 +0800

    Separated template

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-
-<head>
-
-  <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <link rel="profile" href="https://gmpg.org/xfn/11">
-
-  <?php wp_head(); ?>
-
-</head>
-
-<body <?php body_class(); ?>>
-
-  <?php wp_body_open(); ?>
+<?php get_header() ?>
 
   <h1 class="text-3xl font-bold underline">My site here</h1>
-  <?php wp_footer(); ?>
-</body>
-
-</html>
+  
+<?php get_footer() ?>
--- src/js/index.js
+++ src/js/index.js
@@ -16,4 +16,4 @@ function component() {
   return element;
 }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
